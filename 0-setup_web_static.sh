b94794f6088e44caa7ed3e28990487129a396160
Adewale Kujore <adewalekujore6@gmail.com>
2024-04-03 19:52:45 +0100

    task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,4 +13,6 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 
-sed -i "38i \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+sed -i "38i \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+service nginx restart
