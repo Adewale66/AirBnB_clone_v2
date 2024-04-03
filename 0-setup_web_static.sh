0f575ed45c46a027bf57c0c703ee2a45e99c3949
Adewale Kujore <adewalekujore6@gmail.com>
2024-04-03 19:37:00 +0100

    task 1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Sets up web server for deployment of web_static
+
+apt update
+apt install -y nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "Hello World!" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
+sed -i "38i \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
